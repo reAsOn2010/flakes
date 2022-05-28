22f900e2873684977958c4969bd2ec2b9962525e
reAsOn2010 <the.reason.sake@gmail.com>
2022-05-28 11:20:00 +0800

    remove useless code

--- build-env.sh
+++ build-env.sh
@@ -1,19 +1,4 @@
 #! /usr/bin/env bash
 
-mkdir -p /etc/nixos/secrets/users/root
-tr -cd '[:alnum:]' < /dev/urandom | head -c16 | mkpasswd -m sha-512 -s > /etc/nixos/secrets/users/root/hashed-password
-
-while true; do
-  read -s -p "Password: " password
-  echo
-  read -s -p "Password (again): " password2
-  echo
-  [ "$password" = "$password2" ] && break
-  echo "Please try again"
-done
-
-mkdir -p /etc/nixos/secrets/users/yakumo
-echo "$password" | mkpasswd -m sha-512 -s > /etc/nixos/secrets/users/yakumo/hashed-password
-
 nix-shell -p git --run "git clone https://github.com/reAsOn2010/nixos-config.git /tmp/nixos-config"
 nix-shell -p git --run "nixos-rebuild switch --flake \"/tmp/nixos-config#.\""
