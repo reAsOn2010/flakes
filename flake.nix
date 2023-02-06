3b8777c5f9e4f8e0148ff4d594e0fbbb22f96686
yakumo <the.reason.sake@gmail.com>
2023-02-06 11:11:09 +0800

    try add homeage but failed

--- flake.nix
+++ flake.nix
@@ -5,10 +5,27 @@
         nixpkgs = { url = "github:nixos/nixpkgs/nixos-22.11"; };
         home-manager = { url= "github:nix-community/home-manager"; };
         agenix = { url = "github:ryantm/agenix"; };
+        homeage = { url = "github:jordanisaacs/homeage"; };
         flake-utils = { url = "github:numtide/flake-utils"; };
     };
 
-    outputs = inputs@{self, nixpkgs, home-manager, agenix, flake-utils }: {
+    outputs = inputs@{self, nixpkgs, home-manager, agenix, homeage, flake-utils, ... }: {
+        homeManagerConfigurations = {
+          # seems no use...
+          yakumo = home-manager.lib.homeManagerConfiguration {
+            configuration = {
+              homeage = {
+                identityPaths = [ "~/.ssh/id_ed25519" ];
+                installationType = "systemd";
+                file."yakumo-kube-config" = {
+                  source = "./secrets/yakumo/kube-config.age";
+                  copies = [ "/home/yakumo/.kube/config" ];
+                };
+              };
+              imports = [ homeage.homeManagerModules.homeage ];
+            };
+          };
+        };
         nixConfig.substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store/" ];
         nixosConfigurations = {
             pat = inputs.nixpkgs.lib.nixosSystem {
@@ -39,11 +56,28 @@
                 ];
             };
         };
+        # homeManagerConfigurations = {
+        #     pat = home-manager.lib.homeManagerConfiguration {
+        #         configuration = {
+        #             homeage = {
+        #                 identityPaths = [ "~/.ssh/id_ed25519" ];
+        #                 installationType = "systemd";
+        #                 file.".kube/config".source = "./secrets/yakumo/kube-config.age";
+        #             };
+        #             imports = [ homeage.homeManagerModules.homeage ];
+        #         };
+        #     };
+        # };
         devShells."x86_64-linux" = {
           ops = let 
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
           in pkgs.mkShell {
             buildInputs = with pkgs; [
+              pkgs.gcc
+              pkgs.glibc.out
+              pkgs.glibc.static
+              pkgs.libcxx
+              pkgs.lld
               htop
               iotop
             ];
