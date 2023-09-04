cb0fb5ea52ad64576c00832765a8f7517a621564
yakumo <the.reason.sake@gmail.com>
2023-09-04 11:38:56 +0800

    let syncthing dynamic

--- modules/programs/syncthing/default.nix
+++ modules/programs/syncthing/default.nix
@@ -6,16 +6,16 @@
     user = "${user}";
     dataDir = "/home/${user}/Sync";
     configDir = "/home/${user}/.config/syncthing";
-    overrideFolders = true;
-    devices = {
-      "oneplus8p" = { id = "A6LCNPC-BMEOIU7-HPYUKSX-XEYDQ2S-MXZY57Z-P5SDKFK-57YMFA5-FG52KAZ"; };
-    };
-    folders = {
-      "八云的同步文件夹" = {
-        path = "/home/${user}/Sync";
-        devices = [ "oneplus8p" ];
-      };
-    };
+    # overrideFolders = true;
+    # devices = {
+    #   "oneplus8p" = { id = "A6LCNPC-BMEOIU7-HPYUKSX-XEYDQ2S-MXZY57Z-P5SDKFK-57YMFA5-FG52KAZ"; };
+    # };
+    # folders = {
+    #   "八云的同步文件夹" = {
+    #     path = "/home/${user}/Sync";
+    #     devices = [ "oneplus8p" ];
+    #   };
+    # };
   };
   networking.firewall.allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];
