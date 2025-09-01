417feb5be8d1785a92ec909d40cda9a08294b4fb
yakumo <the.reason.sake@gmail.com>
2025-09-02 07:49:31 +0800

    adjust home monitors

--- modules/desktop/hyprland/monitors.nix
+++ modules/desktop/hyprland/monitors.nix
@@ -1,11 +1,11 @@
 { pkgs, hostName, ... }:
 rec {
   primary = {
-    "home" = "HDMI-A-1";
+    "home" = "DP-1";
     "pat" = "HDMI-A-2";
   }."${hostName}";
   left = {
-    "home" = "DP-1";
+    "home" = "HDMI-A-1";
     "pat" = "DP-1";
   }."${hostName}";
   hypr-monitor-conf = pkgs.substitute {
